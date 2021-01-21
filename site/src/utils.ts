ef73997d7de14a765facc2673b4be5f0e6f36fe9
jlkiri <interalia.dev@gmail.com>
2021-01-21 22:46:14 +0900

    More semicolon fix

--- site/src/utils.ts
+++ site/src/utils.ts
@@ -1,14 +1,14 @@
 export async function evaluateJS(input: string) {
-  const blob = new Blob([input], { type: "text/javascript" });
-  const { default: drawCompiled } = await import(URL.createObjectURL(blob));
-  return drawCompiled;
+  const blob = new Blob([input], { type: "text/javascript" })
+  const { default: drawCompiled } = await import(URL.createObjectURL(blob))
+  return drawCompiled
 }
 
 export function debounce(fn: () => void, timeout: number) {
-  let handle;
+  let handle
 
   return () => {
-    clearTimeout(handle);
-    handle = setTimeout(fn, timeout);
-  };
+    clearTimeout(handle)
+    handle = setTimeout(fn, timeout)
+  }
 }
